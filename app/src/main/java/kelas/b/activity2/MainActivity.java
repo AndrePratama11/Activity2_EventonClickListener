bfd10329ecc59becb94b60ab400ed98d084752f7
AndrePratama11 <andre.ong27@gmail.com>
2022-03-15 02:34:27 +0700

    Activity4

--- app/src/main/java/kelas/b/activity2/MainActivity.java
+++ app/src/main/java/kelas/b/activity2/MainActivity.java
@@ -1,14 +1,36 @@
 package kelas.b.activity2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //membuat kondisi jika yang dipilih adalah id mDaftar
+        if (item.getItemId() == R.id.mnDaftar)
+        {
+            //method untuk memanggil activity "MenuActivity"
+            Intent i = new Intent(getApplicationContext(), MenuActivity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Method untuk menampilkan menu
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
     //Deklarasi variabel untuk Button
     Button btnLogin;
 
